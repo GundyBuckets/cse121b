24dd9786eb436a2c048458ca0f7c3cfc62e1532e
GundyBuckets <145598616+GundyBuckets@users.noreply.github.com>
2024-01-17 17:44:12 -0500

    Finished W02 task

---
+++ scripts/w02-task.js
@@ -0,0 +1,45 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Hunter Gundersen";
+let currentYear = 2023;
+let profilePicture = ('images/selfie.jpg');
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector("img");
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = (`<strong>${fullName}</strong>`);
+yearElement.innerHTML = (`${currentYear}`);
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+
+
+/* Step 5 - Array */
+let favoriteFoods = ['Chicken Pot Pie', 'Vanilla Wafers', 'Sour Patch Kids'];
+
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+let anotherFavoriteFood = 'Hamburgers';
+favoriteFoods.push(anotherFavoriteFood);
+
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
